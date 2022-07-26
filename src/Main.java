dea8f6bd6159e691d1fdeb77da00960632095583
Dmitriy <shadowgods@mail.ru>
2022-07-26 18:16:43 +0500

    Четвертое задание обратный вывод

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,52 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int totalSumSpent = 0;
-        for (int daySpending : arr) {
-            totalSumSpent = totalSumSpent + daySpending;
-        }
-        System.out.println(Arrays.toString(arr));
-        System.out.println("Сумма трат за месяц составила " + totalSumSpent + " рублей");
-        int minDaySpent = arr[0];
-        int maxDaySpent = arr[0];
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > maxDaySpent) {
-                maxDaySpent = arr[i];
+//        int totalSumSpent = 0;
+//        for (int daySpending : arr) {
+//            totalSumSpent = totalSumSpent + daySpending;
+//        }
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println("Сумма трат за месяц составила " + totalSumSpent + " рублей");
+//        int minDaySpent = arr[0];
+//        int maxDaySpent = arr[0];
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            if (arr[i] > maxDaySpent) {
+//                maxDaySpent = arr[i];
+//            }
+//            if (arr[i] < minDaySpent) {
+//                minDaySpent = arr[i];
+//            }
+//        }
+//        System.out.println("Минимальная сумма трат за день составила " + minDaySpent + " рублей");
+//        System.out.println("Максимальная сумма трат за день составила " + maxDaySpent + " рублей");
+//        int arrayCount = arr.length;
+//        double averageSum = (double) totalSumSpent / arrayCount;
+//        if (totalSumSpent % arrayCount == 0) {
+//            System.out.println("Средняя сумма трат за месяц составила " + Math.round(averageSum) + " рублей");
+//        } else {
+//            System.out.println("Средняя сумма трат за месяц составила " + averageSum + " рублей");
+//        }
+//
+//    }
+//
+//
+//     static int[] arr = generateRandomArray();
+//
+//    public static int[] generateRandomArray() {
+//        java.util.Random random = new java.util.Random();
+//        int[] arr = new int[30];
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = random.nextInt(100_000) + 100_000;
+//        }
+//        return arr;
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            if (i == reverseFullName.length - 1 ) {
+                System.out.print("\"");
             }
-            if (arr[i] < minDaySpent) {
-                minDaySpent = arr[i];
-            }
+            System.out.print(reverseFullName[i]);
         }
-        System.out.println("Минимальная сумма трат за день составила " + minDaySpent + " рублей");
-        System.out.println("Максимальная сумма трат за день составила " + maxDaySpent + " рублей");
-        int arrayCount = arr.length;
-        double averageSum = (double) totalSumSpent / arrayCount;
-        if (totalSumSpent % arrayCount == 0) {
-            System.out.println("Средняя сумма трат за месяц составила " + Math.round(averageSum) + " рублей");
-        } else {
-            System.out.println("Средняя сумма трат за месяц составила " + averageSum + " рублей");
-        }
-
+        System.out.print("\"");
     }
-
-
-     static int[] arr = generateRandomArray();
-
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-        }
-        return arr;
-    }
-
 
 }
